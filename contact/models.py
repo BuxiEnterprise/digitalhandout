47bf27a5345d144e5777fa24cda8841763bf02eb
Leo <leonardo.ventura@outlook.com>
2019-07-09 01:44:08 -0300

    bkft connect

--- contact/models.py
+++ contact/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-
 class Post(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 
 def init(request):
-    return render(request,'contact/contact.html',{'user:':'Leozinho'})
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'contact/contact.html',{'posts':post})
