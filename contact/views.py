4af31c09600cc38266a42122bb5d1adcd6665bde
Leo <leonardo.ventura@outlook.com>
2019-07-09 01:58:19 -0300

    bkft connect3

--- contact/views.py
+++ contact/views.py
@@ -5,4 +5,4 @@ from django.utils import timezone
 
 def init(request):
     post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'contact/contact.html',{'posts':post})
+    return render(request, '/contact/contact.html',{'posts':post})
