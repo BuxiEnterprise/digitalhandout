b928d24b792c56e7d325696fa7bd8707ebbd3080
Leo <leonardo.ventura@outlook.com>
2019-07-22 19:00:42 -0300

    edit templates and back

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path("",views.init,name="contact")
+    path('',views.init,name="contact"),
+    path('post/<int:pk>/',views.post_detail, name="post_detail")
 ]
